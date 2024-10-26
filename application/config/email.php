a14a7e8954a87cd3691e2cacd1cebd2302ffabfc
maorgeter <maor.geter@lumana.ai>
2024-10-26 19:56:15 +0300

    change email

--- application/config/email.php
+++ application/config/email.php
@@ -5,17 +5,16 @@
 // @link https://codeigniter.com/user_guide/libraries/email.html
 
 $config['useragent'] = 'Easy!Appointments';
-$config['protocol'] = 'mail'; // or 'smtp'
+$config['protocol'] = 'smtp';
 $config['mailtype'] = 'html'; // or 'text'
-// $config['smtp_debug'] = '0'; // or '1'
-// $config['smtp_auth'] = TRUE; //or FALSE for anonymous relay.
-// $config['smtp_host'] = '';
-// $config['smtp_user'] = '';
-// $config['smtp_pass'] = '';
-// $config['smtp_crypto'] = 'ssl'; // or 'tls'
-// $config['smtp_port'] = 25;
-// $config['from_name'] = '';
-// $config['from_address'] = '';
-// $config['reply_to'] = '';
+$config['smtp_auth'] = TRUE; // Enable SMTP authentication
+$config['smtp_host'] = 'smtp.gmail.com';
+$config['smtp_user'] = 'tomermadarbusiness@gmail.com';
+$config['smtp_pass'] = 'T0miBus!1998'; // Replace with your actual password or, ideally, an App Password if 2FA is enabled
+$config['smtp_crypto'] = 'tls'; // Use TLS encryption (or 'ssl' for port 465)
+$config['smtp_port'] = 587; // Port 587 for TLS
+$config['from_name'] = 'Tomi\'s Place';
+$config['from_address'] = 'tomermadarbusiness@gmail.com';
+$config['reply_to'] = 'tomermadarbusiness@gmail.com';
 $config['crlf'] = "\r\n";
 $config['newline'] = "\r\n";
