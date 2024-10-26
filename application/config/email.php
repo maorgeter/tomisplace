5fb647a71cf8895a75901189842d4d4522720e0d
maorgeter <maor.geter@lumana.ai>
2024-10-26 20:30:05 +0300

    update pass

--- application/config/email.php
+++ application/config/email.php
@@ -1,18 +1,15 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed');
 
-// Add custom values by settings them to the $config array.
-// Example: $config['smtp_host'] = 'smtp.gmail.com';
-// @link https://codeigniter.com/user_guide/libraries/email.html
-
 $config['useragent'] = 'Easy!Appointments';
 $config['protocol'] = 'smtp';
-$config['mailtype'] = 'html'; // or 'text'
-$config['smtp_auth'] = TRUE; // Enable SMTP authentication
+$config['mailtype'] = 'html';
+$config['smtp_debug'] = 1; // Enable SMTP debugging (for testing)
+$config['smtp_auth'] = TRUE;
 $config['smtp_host'] = 'smtp.gmail.com';
 $config['smtp_user'] = 'tomermadarbusiness@gmail.com';
-$config['smtp_pass'] = 'T0miBus!1998'; // Replace with your actual password or, ideally, an App Password if 2FA is enabled
-$config['smtp_crypto'] = 'tls'; // Use TLS encryption (or 'ssl' for port 465)
-$config['smtp_port'] = 587; // Port 587 for TLS
+$config['smtp_pass'] = 'ddfh kaob uofa wmaj';
+$config['smtp_crypto'] = 'tls'; // Use TLS encryption
+$config['smtp_port'] = 587;
 $config['from_name'] = 'Tomi\'s Place';
 $config['from_address'] = 'tomermadarbusiness@gmail.com';
 $config['reply_to'] = 'tomermadarbusiness@gmail.com';
